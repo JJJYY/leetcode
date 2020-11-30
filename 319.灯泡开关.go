7b7bed6600e1f8aee72fca9a6dafa42518ca52ad
elevenyang <elevenyang@tencent.com>
2020-11-30 21:20:38 +0800

    feat: update

---
+++ 319.灯泡开关.go
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=319 lang=golang
+ *
+ * [319] 灯泡开关
+ */
+import (
+	"math"
+)
+
+// @lc code=start
+func bulbSwitch(n int) int {
+	return int(math.Sqrt(float64(n)))
+}
+
+// @lc code=end
+
